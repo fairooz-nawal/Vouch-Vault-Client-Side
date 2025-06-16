f88618e6ad6df6b7b95c8bef83d7773911be0a48
Syeda Fairooz Nawal <syedafairooznawal@gmail.com>
2025-06-16 20:31:37 +0600

    Adding Meet Our Partner Section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Banner from '../components/Banner';
-
+import OurPartner from '../components/OurPartner'
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <OurPartner></OurPartner>
         </div>
     );
 };
