37a9797952c9f82e5b7723ed546641686db18620
Syeda Fairooz Nawal <syedafairooznawal@gmail.com>
2025-06-16 18:05:09 +0600

    Adding the footer section

--- src/layout/HomeLayout.jsx
+++ src/layout/HomeLayout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router';
 import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 const HomeLayout = () => {
     return (
         <div className='max-w-full md:max-w-8xl lg:max-w-8xl mx-auto space'>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
