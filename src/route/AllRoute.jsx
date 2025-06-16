6019ee5226200f8ba5a5f3ad4963ab83f42e9068
Syeda Fairooz Nawal <syedafairooznawal@gmail.com>
2025-06-16 17:09:00 +0600

    Initial Setup of the Project is complete

---
+++ src/route/AllRoute.jsx
@@ -0,0 +1,20 @@
+import {
+  createBrowserRouter,
+} from "react-router";
+import HomeLayout from "../layout/HomeLayout";
+import Home from "../pages/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component:HomeLayout,
+    children:[
+        {
+            index:true,
+            Component: Home
+        }
+    ]
+  },
+]);
+
+export default router;
